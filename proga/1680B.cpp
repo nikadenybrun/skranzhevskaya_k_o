0ba5d05289b7296f24e4d8e1c545b2a6ac39b893
nikadenybrun <57352076+nikadenybrun@users.noreply.github.com>
2022-10-13 23:12:11 +0300

    Update 1680B.cpp

--- proga/1680B.cpp
+++ proga/1680B.cpp
@@ -5,20 +5,27 @@ int main() {
 	int n;
 	std::cin >> n;
 	for (int i = 0; i < n; i++) {
-		int l1, r1, l2, r2;
-		std::cout << l1, r1, l2, r2;
-		if (l1 < l2 && r1 < r2) {
-			std::cout << l1 + l2;
+		int left1, r1, left2, r2;
+		std::cin >> left1 >> r1 >> left2 >> r2;
+		if(left1 > left2){
+		    int l_h = 0;
+		    int r_h = 0;
+		    
+		    r_h = r1 + 0;
+		    r1 = r2 + 0;
+		    r2 = r_h + 0;
+		    
+		    l_h = left1 + 0;
+		    left1 = left2 + 0;
+		    left2 = l_h + 0;
 		}
-		else if (l1 > l2 && r1 > r2) {
-			std::cout << l1 + l2;
+		//std::cout << left1 << r1 << left2 << r2 << std::endl;
+		if (r1 < left2) {
+			std::cout << left1 + left2 << std::endl;
+		}else{
+		    std::cout << left2 << std::endl;
 		}
-		else if (l1 <= l2) {
-			std::cout << l2;
-		}
-		else if (l1 > l2) {
-			std::cout << l1;
-		}
+		
 
 
 
