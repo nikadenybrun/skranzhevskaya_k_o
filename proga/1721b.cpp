c244950b6a54d7449e456b833a0170909473fffd
nikadenybrun <57352076+nikadenybrun@users.noreply.github.com>
2022-12-08 22:20:21 +0300

    Update 1721b.cpp

--- proga/1721b.cpp
+++ proga/1721b.cpp
@@ -6,11 +6,13 @@ int main() {
 	int c;
 	std::cin >> c;
 	for (int i = 0; i < c; i++) {
-		int n, m, sx, sy, d;
+		int n = 0;
+		int m, sx, sy, d;
 		std::cin >> n >> m >> sx >> sy >> d;
-		if ((sy + d >= m && sx + d >= n) || (sx - d <= 1 && sy - d <= 1)) {
+		if ((sy+d>=m&&sx+d>=n)||(sx-d<=1&&sy-d<=1) || (sx + d >= n && sx - d <= 0) || (sy + d >= m && sy - d <= 0)) {
 			std::cout << -1 << std::endl;
 		}
+		
 		else {
 			std::cout << n + m - 2 << std::endl;
 		}
